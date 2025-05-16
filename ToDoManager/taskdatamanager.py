7792c66f778253c7efdcd77f704a3f6bed52db8e
DevSaini4752 <devsaini4752@gmail.com>
2025-05-17 04:03:53 +0530

    Adding module for saving task and data

---
+++ ToDoManager/taskdatamanager.py
@@ -0,0 +1,32 @@
+"""Here we will make a function which will add a task to the list, and it will
+be saved in  a JSON file in a "list" form and whenever needed, it will be used
+accordingly"""
+
+#Importing Modules
+import json
+
+# Function
+def mtd(task):
+    data = {}
+    try:
+        with open("data.json", "r") as file_op:
+            data = json.load(file_op)
+
+    # Just to don't raise error during an empty file
+    except json.JSONDecodeError:
+        pass
+    except FileNotFoundError:
+        pass
+
+    #Keeping this out of the first try part because if
+    # an exception is there in try  then it would  end
+    # after  that  execution, and  this  operation  is
+    # compulsory to be executed
+    data.update(task)
+
+    with open("data.json", "w") as file_wr:
+        json.dump(data, file_wr, indent=4)
+
+if __name__ == "__main__":
+    mtd({"Trial" : "Ignore"})
+
