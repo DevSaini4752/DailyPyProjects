0d780167ca5da46e1daf4c3754c54cdfaec8f2d5
DevSaini4752 <devsaini4752@gmail.com>
2025-05-18 04:18:20 +0530

    Handled exception and added indent while saving

--- ToDoManager/pointmanager.py
+++ ToDoManager/pointmanager.py
@@ -3,24 +3,38 @@ It will take  some arguments, and depending on the
 argument, it will add or deduct points.
 
 The point manager will take 2 args, one will be for
-how  many  points to  be added or deducted and the
-time will be taken by manager on itself through time
-module. 2nd would be for what purpose was it deducted.."""
+how  many  points to  be added or deducted, and the
+time will be taken by the manager on itself through time
+module.
+
+Next, 2nd would be for what purpose it was deducted or added."""
 
 #importing module
 import json
 import datetime
+import colours as c
+
 
 # Function for the purpose
 def point_manger(points, purpose):
-    live_time = datetime.datetime.now()
-    with open("data.json", "r") as file:
-        data = json.load(file)
-        data["total_points"] += points
-        history = data["PoiHis"]
-        history[f"{live_time}"] = [f"{points}", f"{purpose}"]
+    try:
+        live_time = datetime.datetime.now()
+        with open("data.json", "r") as file:
+            data = json.load(file)
+            data["total_points"] += points
+            history = data["PoiHis"]
+            history[f"{live_time}"] = [f"{points}", f"{purpose}"]
 
-    with open("data.json", "w") as file:
-        json.dump(data, file)
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent =4)
 
-point_manger(2, "Initializing")
+    except Exception as ex:
+        msg = (f"{c.red}Unexpected error occured -"
+               f"{ex}{c.end}")
+        return msg
+
+
+if __name__ == "__main__":
+    func = point_manger(2, "Initializing")
+    if not func == None:
+        print(func)
